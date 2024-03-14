f5235c12132c93e6e248d2d4f92bbe6017dd49ef
Sunil kumar <sunilkumarbais46@gmail.com>
2024-03-15 00:30:08 +0530

    remove console print

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ let inp = document.querySelector("input")
 inp.style.color = "red";
 let para = document.querySelector("p");
 inp.addEventListener("input",function(){
-    console.log(this.value);
     para.innerText = this.value;
 });
